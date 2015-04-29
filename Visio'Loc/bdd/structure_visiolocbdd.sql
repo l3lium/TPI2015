816e3b34956a8c2292f9af77d6d3f118b9c482da
oliveiras_info <oliveiras_info@CFPI-R105PC24>
2015-04-29 09:14:55 +0200

    Ajout du script da la structure BDD 	- [structure_visiolocbdd.sql]

---
+++ Visio'Loc/bdd/structure_visiolocbdd.sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.4
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mar 28 Avril 2015 à 22:12
+-- Version du serveur :  5.6.15-log
+-- Version de PHP :  5.4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `visiolocbdd`
+--
+CREATE DATABASE IF NOT EXISTS `visiolocbdd` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `visiolocbdd`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `actors`
+--
+
+DROP TABLE IF EXISTS `actors`;
+CREATE TABLE IF NOT EXISTS `actors` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(64) NOT NULL,
+  `lastName` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `creators`
+--
+
+DROP TABLE IF EXISTS `creators`;
+CREATE TABLE IF NOT EXISTS `creators` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(64) NOT NULL,
+  `lastName` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `keywords`
+--
+
+DROP TABLE IF EXISTS `keywords`;
+CREATE TABLE IF NOT EXISTS `keywords` (
+  `id` int(11) NOT NULL,
+  `label` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `label` (`label`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `languages`
+--
+
+DROP TABLE IF EXISTS `languages`;
+CREATE TABLE IF NOT EXISTS `languages` (
+  `id` varchar(2) NOT NULL,
+  `label` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `movies`
+--
+
+DROP TABLE IF EXISTS `movies`;
+CREATE TABLE IF NOT EXISTS `movies` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(128) NOT NULL,
+  `date` date NOT NULL,
+  `imgSrc` varchar(255) NOT NULL,
+  `synopsis` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `movies_has_actors`
+--
+
+DROP TABLE IF EXISTS `movies_has_actors`;
+CREATE TABLE IF NOT EXISTS `movies_has_actors` (
+  `idMovie` int(11) NOT NULL,
+  `idActor` int(11) NOT NULL,
+  PRIMARY KEY (`idMovie`,`idActor`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `movies_has_creators`
+--
+
+DROP TABLE IF EXISTS `movies_has_creators`;
+CREATE TABLE IF NOT EXISTS `movies_has_creators` (
+  `idMovie` int(11) NOT NULL,
+  `idCreator` int(11) NOT NULL,
+  PRIMARY KEY (`idMovie`,`idCreator`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `movies_has_keywords`
+--
+
+DROP TABLE IF EXISTS `movies_has_keywords`;
+CREATE TABLE IF NOT EXISTS `movies_has_keywords` (
+  `idMovie` int(11) NOT NULL,
+  `idKeyword` int(11) NOT NULL,
+  PRIMARY KEY (`idMovie`,`idKeyword`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `movies_has_subtitles`
+--
+
+DROP TABLE IF EXISTS `movies_has_subtitles`;
+CREATE TABLE IF NOT EXISTS `movies_has_subtitles` (
+  `idMovie` int(11) NOT NULL,
+  `idSubtitle` int(11) NOT NULL,
+  `idLanguage` int(11) NOT NULL,
+  PRIMARY KEY (`idMovie`,`idSubtitle`,`idLanguage`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `subtitles`
+--
+
+DROP TABLE IF EXISTS `subtitles`;
+CREATE TABLE IF NOT EXISTS `subtitles` (
+  `id` int(11) NOT NULL,
+  `subSrc` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `userType` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users_has_movies`
+--
+
+DROP TABLE IF EXISTS `users_has_movies`;
+CREATE TABLE IF NOT EXISTS `users_has_movies` (
+  `idUser` int(11) NOT NULL,
+  `idMovie` int(11) NOT NULL,
+  `location` tinyint(1) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idUser`,`idMovie`,`timestamp`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
