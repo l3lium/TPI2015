bd9953cbad42e4c8454b617313c261c1168959e0
oliveiras_info <oliveiras_info@CFPI-R105PC24>
2015-05-05 16:32:12 +0200

    Ajout lien javascript dans la fonction getHeaderHTML [structure.php]

--- Visio'Loc/includes/structure.php
+++ Visio'Loc/includes/structure.php
@@ -9,20 +9,20 @@
  */
 require_once 'specific_funtions.php';
 
-function getHeaderHtml($pageName)
-{
-    echo  
+function getHeaderHtml($pageName) {
+    echo
     "<head>
         <meta charset=\"utf-8\">
         <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
         <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-        <title>Visio'Loc - $pageName</title>
+        <title>$pageName - Visio'Loc</title>
 
         <!-- Bootstrap -->
-        <link href=\"".ROOT_SITE."/css/bootstrap.min.css\" rel=\"stylesheet\">
-        <link href=\"".ROOT_SITE."/css/style.css\" rel=\"stylesheet\">
+        <link href=\"" . ROOT_SITE . "/css/bootstrap.min.css\" rel=\"stylesheet\">
+        <link href=\"" . ROOT_SITE . "/css/style.css\" rel=\"stylesheet\">
         <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js\"></script>
-        <script src=\"".ROOT_SITE."/js/bootstrap.min.js\"></script>
+        <script src=\"" . ROOT_SITE . "/js/bootstrap.min.js\"></script>
+        <script src=\"" . ROOT_SITE . "/js/upload.js\"></script>
     </head>";
 }
 
