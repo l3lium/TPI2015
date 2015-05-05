ff77515af2807ec55ea22b57fa61f0911a1d0957
oliveiras_info <oliveiras_info@CFPI-R105PC24>
2015-05-05 16:36:27 +0200

    Modification du texte des liens [header_base.php]

--- Visio'Loc/includes/struct/header_base.php
+++ Visio'Loc/includes/struct/header_base.php
@@ -13,7 +13,7 @@
                 </button>
                 <a class="navbar-brand" href="<?php echo ROOT_SITE . "/"; ?>index.php">Visio'Loc</a>
             </div>
-            <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
+            <div id="navbar" role="navigation" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
                 <ul class="nav navbar-nav">
                     <li><a href="<?php echo ROOT_SITE . "/"; ?>index.php">Accueil</a></li>
                     <li><a href="#films">Films</a></li>
@@ -37,8 +37,7 @@
                                 <a class="dropdown-toggle" data-toggle="dropdown" href=""><span class="glyphicon glyphicon-user"></span> <?php echo "Administrateur - " . $_SESSION['username']; ?> <span class="caret"></span></a>
                                 <ul class="dropdown-menu">
                                     <li><a class="btn btn-link" href="<?php echo ROOT_SITE; ?>/user/edit.php">Mon compte <span class="glyphicon glyphicon-cog"></span></a></li>
-                                    <li><a class="btn btn-link" href="">Ma vidéothèque <span class="glyphicon glyphicon-facetime-video"></span></a></li>
-                                    <li><a class="btn btn-link" href="">Administration des films <span class="glyphicon glyphicon-film"></span></a></li>
+                                    <li><a class="btn btn-link" href="<?php echo ROOT_SITE . "/"; ?>movie/manage.php">Administration des films <span class="glyphicon glyphicon-film"></span></a></li>
                                     <li><a class="btn btn-link" href="<?php echo ROOT_SITE; ?>/logout.php">Se deconnecter <span class="glyphicon glyphicon-log-out"></span></a></li>
                                 </ul>
                             </li>
@@ -50,7 +49,7 @@
                                 <a class="dropdown-toggle" data-toggle="dropdown" href=""><span class="glyphicon glyphicon-user"></span> <?php echo $_SESSION['username']; ?> <span class="caret"></span></a>
                                 <ul class="dropdown-menu">
                                     <li><a class="btn btn-link" href="<?php echo ROOT_SITE; ?>/user/edit.php">Mon compte <span class="glyphicon glyphicon-cog"></span></a></li>
-                                    <li><a class="btn btn-link" href="">Ma vidéothèque <span class="glyphicon glyphicon-film"></span></a></li>
+                                    <li><a class="btn btn-link" href="<?php echo ROOT_SITE; ?>/user/videotheque.php">Ma vidéothèque <span class="glyphicon glyphicon-film"></span></a></li>
                                     <li><a class="btn btn-link" href="<?php echo ROOT_SITE; ?>/logout.php">Se deconnecter <span class="glyphicon glyphicon-log-out"></span></a></li>
                                 </ul>
                             </li>
